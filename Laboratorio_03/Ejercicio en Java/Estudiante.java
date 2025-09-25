b06f5af38049085998757b3396cd5f9453773ae6
RdrigoFH <rfernandezh@unsa.edu.pe>
2025-09-25 18:43:57 -0500

    Clase estudiante

---
+++ Laboratorio_03/Ejercicio en Java/Estudiante.java
@@ -0,0 +1,35 @@
+public class Estudiante {
+
+}
+
+class Estudiante extends Persona {
+   private String carrera;
+   private double promedio;
+
+   public Estudiante(String nombre, int edad, String carrera, double promedio) {
+      super(nombre, edad);
+      this.carrera = carrera;
+      this.promedio = promedio;
+   }
+
+   public String getCarrera() {
+      return carrera;
+   }
+
+   public void setCarrera(String carrera) {
+      this.carrera = carrera;
+   }
+
+   public double getPromedio() {
+      return promedio;
+   }
+
+   public void setPromedio(double promedio) {
+      this.promedio = promedio;
+   }
+
+   @Override
+   public String toString() {
+      return "Estudiante [" + super.toString() + ", Carrera: " + carrera + ", Promedio: " + promedio + "]";
+   }
+}
