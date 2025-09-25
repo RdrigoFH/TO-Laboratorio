90494cadaca24e5368de2d731526de4309d34b88
RdrigoFH <rfernandezh@unsa.edu.pe>
2025-09-25 18:45:23 -0500

    Clase horario

---
+++ Laboratorio_03/Ejercicio en Java/Horario.java
@@ -0,0 +1,40 @@
+class Horario {
+   private String dia;
+   private String horaInicio;
+   private String horaFin;
+
+   public Horario(String dia, String horaInicio, String horaFin) {
+      this.dia = dia;
+      this.horaInicio = horaInicio;
+      this.horaFin = horaFin;
+   }
+
+   public String getDia() {
+      return dia;
+   }
+
+   public void setDia(String dia) {
+      this.dia = dia;
+   }
+
+   public String getHoraInicio() {
+      return horaInicio;
+   }
+
+   public void setHoraInicio(String horaInicio) {
+      this.horaInicio = horaInicio;
+   }
+
+   public String getHoraFin() {
+      return horaFin;
+   }
+
+   public void setHoraFin(String horaFin) {
+      this.horaFin = horaFin;
+   }
+
+   @Override
+   public String toString() {
+      return "Horario [" + dia + " de " + horaInicio + " a " + horaFin + "]";
+   }
+}
